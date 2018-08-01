809c99ff98fe186c11ab2e7d840a6cd5fe573787
sanshi <zhz961101@gmail.com>
2018-08-01 15:14:54 +0800

    start commit

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
-# S-Vm
-virtual machine
+<h1 align="center">纸上得来终觉浅,欲知此事要躬行</h1>
+
+<h5 align="center"><small>Powered by: Game Scripting Mastery (USA)Alex.Varanese </small></h5>
+
+# README
+面向高级语言的软`虚拟机`，准备用JS和PY都搞搞看
+
+基本上交叉了计科所有内容，大目标的话是实现在这个虚拟机上运行的`lisp方言解释器`
+
+
+# 试水
+```javascript
+var REG = {AX:0,BX:0,CX:0,IP:0,IR:true} // IR:isRuning
+var code = "" // some code
+var STACK = [] // just STACK.pop or push , not set SP option in REG
+
+ver fetch = ()=>code[REG.IP]
+var _eval = function(_inst_){
+    switch(_inst_){
+        // PSH,POP,ADD,SUB,SET,HTL,JMP
+    }
+}
+var run = function(){
+    while(REG.IR){
+        _eval(fetch());
+        REG.IP+=1;
+    }
+}
+```
+结构非常简单，甚至寄存器直接操作的是动态数据，但是结果是很让人兴奋的：
+```javascript
+var code = `SET AX 1
+SET BX 1
+SET CX 10
+ADD AX BX
+DIS AX
+ADD BX AX
+DIS BX
+SUB CX 1
+JMP 3
+HTL`.split("\n")
+```
+（果然汇编不是人写的，那么简单的功能编成这么粗糙的汇编仍然难以看懂）
+<br>CX用来控制循环条件，这个程序将求解斐波那契数第3-13个，并打印出来
+
+# 
---
+++ b/vm.js
@@ -0,0 +1,106 @@
+const debugMODE = false;
+
+var REG = {
+    AX: 0,
+    BX: 0,
+    CX: 0,
+    DX: 0,
+    IP: 0,
+    IR: true
+}
+
+var STACK = []
+
+var code2Line = _c => _c.match(/[A-Z]+( \w+( \w+?)?)?/gi)
+var splitLineVal = _l => {let ret = /[A-Z]+ ((\w+?) ?(\w+?)?)?/gi.exec(_l);console.log(ret,_l);return [ret[2],ret[3],ret[1]];}
+
+// var code = "PSH 5 PSH 5 ADD POP SET AX 9 HTL".split(" ")
+var code = `SET AX 1
+SET BX 1
+SET CX 10
+ADD AX BX
+DIS AX
+ADD BX AX
+DIS BX
+SUB CX 1
+JMP 3
+HTL`.split("\n")
+
+// var code = `PSH 5
+//     PSH 5
+//     ADD
+//     POP BX
+//     ADD DX BX
+//     SET AX 9
+//     HTL`.split("\n")
+
+var log = () => {
+    var _s = "REG\t=> "
+    for (var key in REG) {
+        _s += key + " : " + REG[key] + "; "
+    }
+    _s += "\nSTACK\t=> " + STACK;
+    console.log(_s)
+}
+
+var _fetch = () => code[REG.IP].trim()
+var line_fetch = () => _fetch().split(" ").slice(1)
+var jump = f => REG.IP=parseInt(f)-1;
+
+var _eval = function(_inst_) {
+    // console.log("\n:"+_inst_+"\n",_inst_.split(" ")[0]);
+    let push = val=>STACK.push(val);
+    let pop = ()=>parseInt(STACK.pop());
+    switch (_inst_.split(" ")[0]) {
+        case "PSH":
+            let psh_gett = line_fetch()[0];
+            push(/[A-Z]/g.test(psh_gett)?REG[psh_gett]:psh_gett);
+            break;
+        case "POP":
+            let p = pop();
+            let pop_gett = line_fetch()[0];
+            if(pop_gett && /[A-Z]+/g.test(pop_gett))REG[pop_gett]=p;
+            // console.log(p,_inst_)
+            break;
+        case "ADD":
+            let add_gett = line_fetch();
+            if(add_gett.length!=0) {
+                REG[add_gett[0]]+=/[A-Z]+/g.test(add_gett[1])?parseInt(REG[add_gett[1]]):parseInt(add_gett[1])
+            }
+            else push(pop()+pop())
+            break;
+        case "SUB":
+            let sub_gett = line_fetch();
+            if(sub_gett.length!=0) {
+                REG[sub_gett[0]]-=/[A-Z]+/g.test(sub_gett[1])?parseInt(REG[sub_gett[1]]):parseInt(sub_gett[1])
+            }
+            else push(pop()-pop())
+            break;
+        case "SET":
+            let _kv = line_fetch();
+            REG[_kv[0]] = parseInt(_kv[1]);
+            break;
+        case "DIS":
+            let _disp = line_fetch();
+            if(/[A-Z]+/g.test(_disp[0])) console.log(REG[_disp[0]])
+            else console.log(_disp[0])
+            break;
+        case "JMP":
+            let _f = line_fetch();
+            if(REG.CX>0)jump(_f[0]);
+            break;
+        case "HTL":
+            REG.IR = false;
+            break;
+    }
+}
+
+var run = function() {
+    while (REG.IR) {
+        _eval(_fetch());
+        REG.IP+=1;
+        debugMODE && log();
+    }
+}
+
+run()
